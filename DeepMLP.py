8bb8e03e9acf7db416e0df2490f7844c43a35b8e
sshworld <ssh97540@gmail.com>
2021-06-09 18:59:57 +0900

    DeepMLP

---
+++ DeepMLP.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+
+x_train, x_test, y_train, y_test = np.load('./img_data.npy', allow_pickle=True)
+
+mlp = MLPClassifier(hidden_layer_sizes=(716, 1000, 1000, 10), learning_rate_init=0.0005, batch_size=32, solver='adam',
+                    verbose=True, max_iter=100)
+
+
+mlp.fit(x_train, y_train)
+res = mlp.predict(x_test)
+
+print(len(res))
+
+conf = np.zeros((36, 36))
+
+for i in range(len(res)) :
+    conf[res[i]][y_test[i]] += 1
+print(conf)
+
+correct = 0
+for i in range(36) :
+    correct += conf[i][i]
+accuracy = correct / len(res)
+print(accuracy*100)
